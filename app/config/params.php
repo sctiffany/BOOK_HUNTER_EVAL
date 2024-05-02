2588f0ec2418f76a69cbb2e75ff7f8a1cade3f20
sctiffany <tiffany.schinelli@gmail.com>
2024-05-02 15:52:14 +0200

    Intégration et décomposition du template en partials

--- app/config/params.php
+++ app/config/params.php
@@ -1,4 +1,11 @@
 <?php
+// PARAMS
+
+// Initialisation des zones dynamiques
+$title = '';
+$content = '';
+
+// Paramètres de connexion à la DB
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'book_hunter_2023');
 define('DB_USER', 'root');
---
+++ app/views/templates/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <?php include_once '../app/views/templates/partials/_head.php'; ?>
+  </head>
+  <body class="bg-gray-900 text-white font-sans leading-normal tracking-normal">
+    <!-- Header -->
+    <?php include_once '../app/views/templates/partials/_nav.php'; ?>
+
+    <!-- Main -->
+    <div class="container mx-auto flex flex-wrap pt-4 pb-12 text-white">
+        
+      <!-- Aside -->
+      <?php include_once '../app/views/templates/partials/_aside.php'; ?>
+
+      <!-- Main content -->
+      <?php include_once '../app/views/templates/partials/_main.php'; ?>
+    </div>
+
+    <!-- Footer -->
+    <?php include_once '../app/views/templates/partials/_footer.php'; ?>
+  </body>
+</html>
