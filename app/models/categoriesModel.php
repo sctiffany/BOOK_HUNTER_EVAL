cc0169b06cb27aeb2a48626b3089133d8d32e869
sctiffany <tiffany.schinelli@gmail.com>
2024-05-16 10:59:56 +0200

    Categories._index

---
+++ app/models/categoriesModel.php
@@ -0,0 +1,10 @@
+<?php 
+namespace App\Models\CategoriesModel;
+use \PDO;
+
+function findAll (PDO $connexion): array {
+    $sql = "SELECT *
+            FROM categories
+            ORDER BY name ASC;";
+    return $connexion->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}       
